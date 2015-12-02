d35ef2dc0a6a58dc3f37b6d3b7ea7f33535b6635
Zito Antonio <antonio.zito@sia.eu>
2015-12-02 17:52:05 +0100

    modifica metodo welcomePage per lettura parametri di request

--- web/src/main/java/eu/sia/sialabs/controller/SiaLabsController.java
+++ web/src/main/java/eu/sia/sialabs/controller/SiaLabsController.java
@@ -1,16 +1,18 @@
 package eu.sia.sialabs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SiaLabsController {
 
 	@RequestMapping("/welcome")
-	public ModelAndView welcomePage() {
-		String message = "<br><div style='text-align:center;'>"
-				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>"
+	public ModelAndView welcomePage(@RequestParam(value="firstName", required=false) String firstName, @RequestParam(value="lastName", required=false) String lastName) {
+		String message = "<br><div style='text-align:center;'>" 
+				+ "<h3>********** Hello " + firstName + " " + lastName + ", Spring MVC Tutorial</h3>"
 				+ "This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
 		return new ModelAndView("welcome", "message", message);
 	}
