e9f45fa2943ca03387a0b1aadb1eae54b606f45e
foogaro <l.fugaro@gmail.com>
2015-12-03 15:16:39 +0100

    Aggiunte le dependecies Hipernate e JPA.

---
+++ web/src/main/java/it/redhat/sia/labs/model/entity/UtenteEntity.java
@@ -0,0 +1,79 @@
+package it.redhat.sia.labs.model.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by foogaro on 03/12/15.
+ */
+@Entity
+@Table(name = "UTENTE", schema = "SIALABS", catalog = "")
+public class UtenteEntity {
+    private int id;
+    private String nome;
+    private String cognome;
+    private String sesso;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "nome")
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Basic
+    @Column(name = "cognome")
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    @Basic
+    @Column(name = "sesso")
+    public String getSesso() {
+        return sesso;
+    }
+
+    public void setSesso(String sesso) {
+        this.sesso = sesso;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UtenteEntity that = (UtenteEntity) o;
+
+        if (id != that.id) return false;
+        if (nome != null ? !nome.equals(that.nome) : that.nome != null) return false;
+        if (cognome != null ? !cognome.equals(that.cognome) : that.cognome != null) return false;
+        if (sesso != null ? !sesso.equals(that.sesso) : that.sesso != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (nome != null ? nome.hashCode() : 0);
+        result = 31 * result + (cognome != null ? cognome.hashCode() : 0);
+        result = 31 * result + (sesso != null ? sesso.hashCode() : 0);
+        return result;
+    }
+}
